d7aff31625cc9d84c9a5b5de59d9876c35345a0a
GitHub Action <action@github.com>
2023-05-22 15:53:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/floholz/adbconnect/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/floholz/adbconnect/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.floholz.adbconnect.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
